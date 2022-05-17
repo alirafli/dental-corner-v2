a3c4e2076e655424bed7b61cf67bfae9e5da9ba4
daffayanuardhana <daffayanuardhana@gmail.com>
2022-05-17 13:53:22 +0700

    daffa [ADD] profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Helmet from "react-helmet";
+import { Input, Header, Button } from "../../components";
+import DUMMY from "../../assets/img/pp-dummy.jpg";
 
 const Profile = () => {
   return (
@@ -7,7 +9,29 @@ const Profile = () => {
       <Helmet>
         <title>Dental Corner | Profile</title>
       </Helmet>
-      Profile
+      <div className="flex flex-col items-center">
+        <div className="flex">
+          <div className="w-1/2 ">
+            <img src={DUMMY} alt="Dummy" className="rounded-full" />
+          </div>
+          <div className="w-full  font-black mt-10 ml-10">
+            <Header bold h3>
+              Catherine Vania
+            </Header>
+            <Button className="w-fit mt-3" primary>
+              Edit Profile
+            </Button>
+          </div>
+        </div>
+        <div className="w-1/2 mt-7">
+          <Input type="text">Nama</Input>
+          <Input>Email</Input>
+          <Input>Password</Input>
+          <Input>Birth Date</Input>
+          <Input>Address</Input>
+          <Input>Nomor Telepon</Input>
+        </div>
+      </div>
     </div>
   );
 };
