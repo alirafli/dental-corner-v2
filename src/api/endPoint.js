69ecfc24d1f33de0e1aaffa78e3ff68377c34b65
alirafli <mrafli1212@gmail.com>
2022-05-18 23:55:19 +0700

    ali [MODIFY] endpoint

--- src/api/endPoint.js
+++ src/api/endPoint.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://shrouded-sea-21115.herokuapp.com/api",
   header: {
     "Access-Control-Allow-Origin": "*",
   },
